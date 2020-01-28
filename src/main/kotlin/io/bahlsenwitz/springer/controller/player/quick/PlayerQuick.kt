230e4ea06a0eb3f35511454c2e13a077f067a701
S. Matthew English <s.matthew.english@gmail.com>
2020-01-28 15:02:17 -0500

    ~

---
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/player/quick/PlayerQuick.kt
@@ -0,0 +1,20 @@
+package io.bahlsenwitz.springer.controller.player.quick
+
+import io.bahlsenwitz.springer.model.Player
+import io.bahlsenwitz.springer.repository.RepositoryPlayer
+import org.springframework.http.ResponseEntity
+import java.util.*
+
+class PlayerQuick(private val repositoryPlayer: RepositoryPlayer) {
+
+    //
+    //TODO: need also consider how many ongoing games they have, if we have a mutual ongoing game, etc...
+    //
+    fun quick(id: String): ResponseEntity<Any> {
+        val uuid: UUID = UUID.fromString(id)!!
+        val playerListTopTen: List<Player> = repositoryPlayer.findAll().filter { it.id != uuid }.sorted().take(11)
+        val opponent: Player = playerListTopTen.shuffled().take(1)[0]
+        return ResponseEntity.ok(opponent)
+    }
+
+}
