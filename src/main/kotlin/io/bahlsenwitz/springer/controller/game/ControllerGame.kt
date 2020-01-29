564c58905e84b481396c14b825b8d96fc2c4dd21
S. Matthew English <s.matthew.english@gmail.com>
2020-01-28 20:02:40 -0500

    optimp

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/ControllerGame.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/ControllerGame.kt
@@ -1,7 +1,6 @@
 package io.bahlsenwitz.springer.controller.game
 
 import io.bahlsenwitz.springer.controller.game.test.GameTest
-import io.bahlsenwitz.springer.model.Game
 import io.bahlsenwitz.springer.repository.RepositoryGame
 import io.bahlsenwitz.springer.repository.RepositoryPlayer
 import org.springframework.beans.factory.annotation.Autowired
