dcbca13ab1ca1fa1646dc8838515b33457bed87f
S. Matthew English <s.matthew.english@gmail.com>
2020-02-07 21:31:42 -0500

    up

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/test/GameTest.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/test/GameTest.kt
@@ -1,5 +1,6 @@
 package io.bahlsenwitz.springer.controller.game.test
 
+import io.bahlsenwitz.springer.generator.util.GeneratorDateTime
 import io.bahlsenwitz.springer.model.game.Game
 import io.bahlsenwitz.springer.repository.RepositoryGame
 import org.springframework.http.ResponseEntity
@@ -8,12 +9,15 @@ import java.util.*
 //curl --header "Content-Type: application/json" --request POST --data '{"state":[[""]]}' http://localhost:8080/game/test
 class GameTest(private val repositoryGame: RepositoryGame) {
 
+    private val DATE_TIME_GENERATOR = GeneratorDateTime()
+
     fun test(requestTest: RequestTest): ResponseEntity<Any> {
         val id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
         val game: Game = repositoryGame.findById(id).get()
         if(requestTest.state != arrayListOf(arrayListOf(""))){
             game.state = requestTest.state
         }
+        game.updated = DATE_TIME_GENERATOR.rightNowString()
         return ResponseEntity.ok(repositoryGame.save(game))
     }
 
