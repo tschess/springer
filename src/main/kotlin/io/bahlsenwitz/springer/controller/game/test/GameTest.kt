c986237f455fbbc55382f4f20be919cff2abb51f
S. Matthew English <s.matthew.english@gmail.com>
2020-02-07 23:58:02 -0500

    t

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/test/GameTest.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/test/GameTest.kt
@@ -15,7 +15,10 @@ class GameTest(private val repositoryGame: RepositoryGame) {
     fun test(requestTest: RequestTest): ResponseEntity<Any> {
         val id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000")
         val game: Game = repositoryGame.findById(id).get()
-        if(requestTest.state != arrayListOf(arrayListOf(""))){
+        if(requestTest.state == arrayListOf(arrayListOf("DEFAULT"))){
+            game.state = defaultState()
+        }
+        else if(requestTest.state != arrayListOf(arrayListOf(""))){
             game.state = requestTest.state
         }
         if(requestTest.turn == "WHITE"){
@@ -32,4 +35,54 @@ class GameTest(private val repositoryGame: RepositoryGame) {
         val state: List<List<String>>,
         val turn: String
     )
+
+    private fun defaultState(): List<List<String>> {
+        val row0: List<String> =
+            arrayListOf(
+                "RookWhite",
+                "KnightWhite",
+                "BishopWhite",
+                "QueenWhite",
+                "KingWhite",
+                "BishopWhite",
+                "KnightWhite",
+                "RookWhite"
+            )
+        val row1: List<String> = arrayListOf(
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite",
+            "PawnWhite"
+        )
+        val row2: List<String> = arrayListOf("", "", "", "", "", "", "", "")
+        val row3: List<String> = arrayListOf("", "", "", "", "", "", "", "")
+        val row4: List<String> = arrayListOf("", "", "", "", "", "", "", "")
+        val row5: List<String> = arrayListOf("", "", "", "", "", "", "", "")
+        val row6: List<String> = arrayListOf(
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack",
+            "PawnBlack"
+        )
+        val row7: List<String> =
+            arrayListOf(
+                "RookBlack",
+                "KnightBlack",
+                "BishopBlack",
+                "QueenBlack",
+                "KingBlack",
+                "BishopBlack",
+                "KnightBlack",
+                "RookBlack"
+            )
+        return arrayListOf(row0, row1, row2, row3, row4, row5, row6, row7)
+    }
 }
