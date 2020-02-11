800ba65145ec61758f1bfa137b35ca90ea8f16fd
S. Matthew English <s.matthew.english@gmail.com>
2020-02-10 22:00:23 -0500

    rev

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/ack/GameAck.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/ack/GameAck.kt
@@ -94,14 +94,14 @@ class GameAck(
 
         fun generateState(config: List<List<String>>, state: List<List<String>>): List<List<String>> {
             return arrayListOf(
-                setOrientation(row = state[0], color = "Black"),
-                setOrientation(row = state[1], color = "Black"),
+                setOrientation(row = config[0], color = "Black"),
+                setOrientation(row = config[1], color = "Black"),
                 arrayListOf("", "", "", "", "", "", "", ""),
                 arrayListOf("", "", "", "", "", "", "", ""),
                 arrayListOf("", "", "", "", "", "", "", ""),
                 arrayListOf("", "", "", "", "", "", "", ""),
-                setOrientation(row = config[1], color = "White"),
-                setOrientation(row = config[0], color = "White")
+                setOrientation(row = state[1], color = "White"),
+                setOrientation(row = state[0], color = "White")
             )
         }
     }
