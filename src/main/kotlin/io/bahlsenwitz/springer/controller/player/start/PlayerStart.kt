1a2361160a7f6d604cb729fbe50a4c907479a995
S. Matthew English <s.matthew.english@gmail.com>
2020-02-24 20:47:56 -0500

    o

--- src/main/kotlin/io/bahlsenwitz/springer/controller/player/start/PlayerStart.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/player/start/PlayerStart.kt
@@ -30,7 +30,7 @@ class PlayerStart(private val repositoryPlayer: RepositoryPlayer) {
             ?: return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("{\"error\": \"nonexistent\"}")
 
         //khttp.post(url = "http://3.12.121.89:8086/write?db=tschess", data = "activity player=0101010")
-        khttp.post(url = "http://localhost:8086/write?db=tschess", data = "slaps id=${player.id}")
+        khttp.post(url = "http://3.12.121.89:8086/write?db=zzz", data = "slaps id=${player.id}")
 
         if (BCryptPasswordEncoder().matches(password, player.password)) {
             player.device = device
