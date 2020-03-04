250dde10699d7882ee5d364ea0ba73d324763fba
S. Matthew English <s.matthew.english@gmail.com>
2020-03-04 16:42:20 -0500

    x

--- src/main/kotlin/io/bahlsenwitz/springer/generator/common/Default.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/generator/common/Default.kt
@@ -5,49 +5,49 @@ class Default {
         fun state(): List<List<String>> {
             val row0: List<String> =
                 arrayListOf(
-                    "RookWhite",
-                    "KnightWhite",
-                    "BishopWhite",
-                    "QueenWhite",
-                    "KingWhite",
-                    "BishopWhite",
-                    "KnightWhite",
-                    "RookWhite"
+                    "RookWhite_x",
+                    "KnightWhite_x",
+                    "BishopWhite_x",
+                    "QueenWhite_x",
+                    "KingWhite_x",
+                    "BishopWhite_x",
+                    "KnightWhite_x",
+                    "RookWhite_x"
                 )
             val row1: List<String> = arrayListOf(
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite",
-                "PawnWhite"
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x",
+                "PawnWhite_x"
             )
             val row2: List<String> = arrayListOf("", "", "", "", "", "", "", "")
             val row3: List<String> = arrayListOf("", "", "", "", "", "", "", "")
             val row4: List<String> = arrayListOf("", "", "", "", "", "", "", "")
             val row5: List<String> = arrayListOf("", "", "", "", "", "", "", "")
             val row6: List<String> = arrayListOf(
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack",
-                "PawnBlack"
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x",
+                "PawnBlack_x"
             )
             val row7: List<String> =
                 arrayListOf(
-                    "RookBlack",
-                    "KnightBlack",
-                    "BishopBlack",
-                    "QueenBlack",
-                    "KingBlack",
-                    "BishopBlack",
-                    "KnightBlack",
-                    "RookBlack"
+                    "RookBlack_x",
+                    "KnightBlack_x",
+                    "BishopBlack_x",
+                    "QueenBlack_x",
+                    "KingBlack_x",
+                    "BishopBlack_x",
+                    "KnightBlack_x",
+                    "RookBlack_x"
                 )
             return arrayListOf(row0, row1, row2, row3, row4, row5, row6, row7)
         }
--- src/main/kotlin/io/bahlsenwitz/springer/generator/test/GeneratorTestGameAct.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/generator/test/GeneratorTestGameAct.kt
@@ -1,5 +1,6 @@
 package io.bahlsenwitz.springer.generator.test
 
+import io.bahlsenwitz.springer.generator.common.Default
 import io.bahlsenwitz.springer.model.game.CONTESTANT
 import io.bahlsenwitz.springer.model.game.Game
 import io.bahlsenwitz.springer.model.game.STATUS
@@ -16,60 +17,10 @@ class GeneratorTestGameAct(
             id = UUID.fromString("00000000-0000-0000-0000-000000000000")!!,
             white = generatorTestPlayer.findByName(username = "white"),
             black = generatorTestPlayer.findByName(username = "black"),
-            state = defaultState(),
+            state = Default.state(),
             challenger = CONTESTANT.WHITE,
             status = STATUS.ONGOING
         )
         repositoryGame.save(test)
-    }
-
-    private fun defaultState(): List<List<String>> {
-        val row0: List<String> =
-            arrayListOf(
-                "RookWhite_x",
-                "KnightWhite_x",
-                "BishopWhite_x",
-                "QueenWhite_x",
-                "KingWhite_x",
-                "BishopWhite_x",
-                "KnightWhite_x",
-                "RookWhite_x"
-            )
-        val row1: List<String> = arrayListOf(
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x",
-            "PawnWhite_x"
-        )
-        val row2: List<String> = arrayListOf("", "", "", "", "", "", "", "")
-        val row3: List<String> = arrayListOf("", "", "", "", "", "", "", "")
-        val row4: List<String> = arrayListOf("", "", "", "", "", "", "", "")
-        val row5: List<String> = arrayListOf("", "", "", "", "", "", "", "")
-        val row6: List<String> = arrayListOf(
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x",
-            "PawnBlack_x"
-        )
-        val row7: List<String> =
-            arrayListOf(
-                "RookBlack_x",
-                "KnightBlack_x",
-                "BishopBlack_x",
-                "QueenBlack_x",
-                "KingBlack_x",
-                "BishopBlack_x",
-                "KnightBlack_x",
-                "RookBlack_x"
-            )
-        return arrayListOf(row0, row1, row2, row3, row4, row5, row6, row7)
     }
 }
