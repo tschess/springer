e74f351e4a0167b1f52475f5e35d25b89c64f8f6
smatthewenglish <s.matthew.english@gmail.com>
2020-05-28 16:18:36 -0400

    nice

--- src/main/kotlin/io/bahlsenwitz/springer/push/Pusher.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/push/Pusher.kt
@@ -1,22 +1,52 @@
 package io.bahlsenwitz.springer.push
 
+import io.bahlsenwitz.springer.model.player.Player
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+
+
 class Pusher {
 
-    /**
-     *  iOS
-     *
-     *  https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server
-     *  https://developer.apple.com/documentation/usernotifications/setting_up_a_remote_notification_server/sending_notification_requests_to_apns/
-     *
-     */
-    fun ios(){
-        //TODO: soLaLa...
+    private val development: String = "api.sandbox.push.apple.com:443"
+    private val production: String = "api.push.apple.com:443"
+
+    private val key: Key = Key()
+
+    fun ios(player: Player) {
+//soLaLa
+        //headers: Map<String, String> = mapOf(), params: Map<String, String> = mapOf(),
+
+        val path: String = "/3/device/"
+
+        try {
+            khttp.post(
+                headers = "",
+                url = "$development$path${player.note_key}",
+                data =
+            )
+        } catch (e: Exception) {
+            print(e.localizedMessage)
+        }
     }
-    //Establish a Connection to APNs
-    //
-    //Use HTTP/2 and TLS 1.2 or later to establish a connection between your provider server and one of the following servers:
-    //
-    //    Development server: api.sandbox.push.apple.com:443
-    //
-    //    Production server: api.push.apple.com:443
+
+    fun getSha256Hash(): String? {
+        return try {
+            var digest: MessageDigest? = null
+            try {
+                digest = MessageDigest.getInstance("SHA-256")
+            } catch (e1: NoSuchAlgorithmException) {
+                e1.printStackTrace()
+            }
+            digest!!.reset()
+            bin2hex(digest.digest(key.value.toByteArray()))
+        } catch (ignored: java.lang.Exception) {
+            null
+        }
+    }
+
+    private fun bin2hex(data: ByteArray): String? {
+        val hex = StringBuilder(data.size * 2)
+        for (b: Byte in data) hex.append(String.format("%02x", (b.toInt() and 0x0f).toByte()))
+        return hex.toString()
+    }
 }
