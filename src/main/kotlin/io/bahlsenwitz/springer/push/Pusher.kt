e174cc8135771d24027f68076652112ef94a2f2f
smatthewenglish <s.matthew.english@gmail.com>
2020-05-28 19:27:58 -0400

    pathfinder

--- src/main/kotlin/io/bahlsenwitz/springer/push/Pusher.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/push/Pusher.kt
@@ -6,7 +6,7 @@ class Pusher() {
     fun test() {
 
         val command = listOf(
-            "~/springer/src/main/kotlin/io/bahlsenwitz/springer/push/pu.sh",
+            "/home/ubuntu/springer/src/main/kotlin/io/bahlsenwitz/springer/push/pu.sh",
             "9c06a3a757faf8ee173fb8f6b631160db1a2dd8c43f3f4a083d3dbb88034a76a",
             "{\"aps\":{\"alert\":\"your move...42\",\"badge\":\"1\",\"content-available\":1}}"
         )
