ca1f20a5a7f37df1f14e7ffc7c032cbf7fd99a61
smatthewenglish <s.matthew.english@gmail.com>
2020-05-27 20:05:45 -0400

    //TODO: soLaLa...

---
+++ src/main/kotlin/io/bahlsenwitz/springer/push/Pusher.kt
@@ -0,0 +1,18 @@
+package io.bahlsenwitz.springer.push
+
+class Pusher {
+
+    /**
+     *  iOS
+     */
+    fun ios(){
+        //TODO: soLaLa...
+    }
+    //Establish a Connection to APNs
+    //
+    //Use HTTP/2 and TLS 1.2 or later to establish a connection between your provider server and one of the following servers:
+    //
+    //    Development server: api.sandbox.push.apple.com:443
+    //
+    //    Production server: api.push.apple.com:443
+}
