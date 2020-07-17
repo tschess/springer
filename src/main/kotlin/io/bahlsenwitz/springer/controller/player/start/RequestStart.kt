9d8750f98842b5e0acb5669b022f10848b570519
smatthewenglish <s.matthew.english@gmail.com>
2020-07-17 17:09:04 -0400

    ~

--- src/main/kotlin/io/bahlsenwitz/springer/controller/player/start/RequestStart.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/player/start/RequestStart.kt
@@ -3,6 +3,5 @@ package io.bahlsenwitz.springer.controller.player.start
 data class RequestStart(
     val username: String,
     val password: String,
-    val device: String,
-    val note_key: String
+    val device: String
 )
