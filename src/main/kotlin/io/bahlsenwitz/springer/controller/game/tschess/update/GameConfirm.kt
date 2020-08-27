4b4d58e143bf64b0cfa3c2f1cd101e35d04203d5
smatthewenglish <s.matthew.english@gmail.com>
2020-08-27 13:09:25 -0400

    confirm route

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
@@ -1,56 +1,25 @@
 package io.bahlsenwitz.springer.controller.game.tschess.update
 
 import io.bahlsenwitz.springer.controller.Output
-import io.bahlsenwitz.springer.model.game.CONDITION
-import io.bahlsenwitz.springer.model.game.CONTESTANT
 import io.bahlsenwitz.springer.model.game.Game
-import io.bahlsenwitz.springer.model.game.STATUS
-import io.bahlsenwitz.springer.model.player.Player
 import io.bahlsenwitz.springer.repository.RepositoryGame
-import io.bahlsenwitz.springer.repository.RepositoryPlayer
-import io.bahlsenwitz.springer.util.ConfigState
-import io.bahlsenwitz.springer.util.DateTime
-import io.bahlsenwitz.springer.util.Rating
 import org.springframework.http.ResponseEntity
 import java.util.*
 
-class GameConfirm(
-    private val repositoryGame: RepositoryGame,
-    private val repositoryPlayer: RepositoryPlayer
-) {
+class GameConfirm(private val repositoryGame: RepositoryGame) {
 
-    private val dateTime: DateTime = DateTime()
-    private val configState: ConfigState = ConfigState()
-    private val output: Output =
-        Output(repositoryGame = repositoryGame)
-    private val rating: Rating = Rating(repositoryGame, repositoryPlayer)
+    private val output: Output = Output(repositoryGame = repositoryGame)
 
-    data class UpdateResign(
+    data class UpdateConfirm(
         val id_game: String,
-        val id_self: String,
         val white: Boolean
     )
 
-    fun resign(updateResign: UpdateResign): ResponseEntity<Any>? {
-        val game: Game = repositoryGame.findById(UUID.fromString(updateResign.id_game)!!).get()
-        if (game.status == STATUS.RESOLVED) {
-            return output.terminal(result = "fail", route = "resign")
-        }
-        val date: String = dateTime.getDate()
-        val playerSelf: Player = repositoryPlayer.findById(UUID.fromString(updateResign.id_self)!!).get()
-        playerSelf.updated = date
-        playerSelf.note_value = false
-        repositoryPlayer.save(playerSelf)
-        game.highlight = "TBD"
-        game.status = STATUS.RESOLVED
-        game.condition = CONDITION.RESIGN
-        game.state = configState.poisonReveal(game.state!!)
-        if(game.white == playerSelf){
-            game.turn = CONTESTANT.WHITE
-        } else {
-            game.turn = CONTESTANT.BLACK
-        }
-        rating.resolve(game)
-        return output.update(route = "resign", game = game)
+    fun confirm(updateConfirm: UpdateConfirm): ResponseEntity<Any>? {
+        val game: Game = repositoryGame.findById(UUID.fromString(updateConfirm.id_game)!!).get()
+
+
+
+        return output.update(route = "confirm", game = game)
     }
 }
