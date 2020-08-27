aad60bc79224cf4218442fbb2b684ed79fb3a18f
smatthewenglish <s.matthew.english@gmail.com>
2020-08-27 13:29:58 -0400

    async confirm

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
@@ -1,6 +1,7 @@
 package io.bahlsenwitz.springer.controller.game.tschess.update
 
 import io.bahlsenwitz.springer.controller.Output
+import io.bahlsenwitz.springer.model.game.CONFIRM
 import io.bahlsenwitz.springer.model.game.Game
 import io.bahlsenwitz.springer.repository.RepositoryGame
 import org.springframework.http.ResponseEntity
@@ -17,9 +18,18 @@ class GameConfirm(private val repositoryGame: RepositoryGame) {
 
     fun confirm(updateConfirm: UpdateConfirm): ResponseEntity<Any>? {
         val game: Game = repositoryGame.findById(UUID.fromString(updateConfirm.id_game)!!).get()
-
-
-
+        if(game.confirm == null){
+            return output.terminal(result = "fail", route = "confirm")
+        }
+        if(game.confirm!! == CONFIRM.WHITE_BLACK){
+            if(updateConfirm.white){
+                game.confirm = CONFIRM.BLACK
+                return output.update(route = "confirm", game = game)
+            }
+            game.confirm = CONFIRM.WHITE
+            return output.update(route = "confirm", game = game)
+        }
+        game.confirm = null
         return output.update(route = "confirm", game = game)
     }
 }
