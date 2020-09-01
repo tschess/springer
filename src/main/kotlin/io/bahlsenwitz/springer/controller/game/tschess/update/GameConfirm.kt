36c1f535342f75be4594e30a13cde96c078150ad
smatthewenglish <s.matthew.english@gmail.com>
2020-09-01 15:09:00 -0400

    RESOLVED_WHITE_BLACK

--- src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
+++ src/main/kotlin/io/bahlsenwitz/springer/controller/game/tschess/update/GameConfirm.kt
@@ -1,8 +1,8 @@
 package io.bahlsenwitz.springer.controller.game.tschess.update
 
 import io.bahlsenwitz.springer.controller.Output
-import io.bahlsenwitz.springer.model.game.CONFIRM
 import io.bahlsenwitz.springer.model.game.Game
+import io.bahlsenwitz.springer.model.game.STATUS
 import io.bahlsenwitz.springer.repository.RepositoryGame
 import org.springframework.http.ResponseEntity
 import java.util.*
@@ -18,18 +18,18 @@ class GameConfirm(private val repositoryGame: RepositoryGame) {
 
     fun confirm(updateConfirm: UpdateConfirm): ResponseEntity<Any> {
         val game: Game = repositoryGame.findById(UUID.fromString(updateConfirm.id_game)!!).get()
-        if(game.confirm == null){
+        if(game.status == STATUS.RESOLVED){
             return output.terminal(result = "fail", route = "confirm")
         }
-        if(game.confirm!! == CONFIRM.WHITE_BLACK){
+        if(game.status == STATUS.RESOLVED_WHITE_BLACK){
             if(updateConfirm.white){
-                game.confirm = CONFIRM.BLACK
+                game.status = STATUS.RESOLVED_BLACK
                 return output.update(route = "confirm", game = game)
             }
-            game.confirm = CONFIRM.WHITE
+            game.status = STATUS.RESOLVED_WHITE
             return output.update(route = "confirm", game = game)
         }
-        game.confirm = null
+        game.status = STATUS.RESOLVED
         return output.update(route = "confirm", game = game)
     }
 }
